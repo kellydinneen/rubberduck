e62727f500d9120ed1cf8b44fee90f3102a35f8a
kellydinneen <kgdinneen@gmail.com>
2021-03-07 10:12:20 -0700

    Create prescription data structure

---
+++ src/prescriptionData.js
@@ -0,0 +1,27 @@
+export const prescriptions = {
+  thinkingTools: {
+    oblique: [],
+    research: [],
+    sciency: [],
+    softy: []
+  },
+  randR:{
+    rest: [],
+    calm: [],
+    movement: [],
+    nourishment: []
+  },
+  esteem: {
+    cliche: [],
+    curious: [],
+    selfAdministered: [],
+  },
+  laughter: {
+
+  },
+  toughLove: {
+    fromDuck: [],
+    fromTube: [],
+    selfAdministered: []
+  }
+}
