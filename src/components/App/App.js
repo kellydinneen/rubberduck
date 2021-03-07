f9e73faaf24b6c6a1a889fd222f2c59b9fc59ec7
kellydinneen <kgdinneen@gmail.com>
2021-03-06 21:01:20 -0700

    Define routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Switch, Route } from "react-router-dom";
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Home from '../Home/Home';
+import Form from '../Form/Form';
+import Prescription from '../Prescription/Prescription';
+import About from '../About/About';
+import SolutionIndex from '../SolutionIndex/SolutionIndex';
+import ErrorPage from '../ErrorPage/ErrorPage'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
-export default App;
+  return (
+      <>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <Header home={true}/>
+                <Home />
+                <Footer />
+              </>
+            )}
+          />
+          <Route
+            path="/form/:user"
+            render={() => (
+              <>
+                <Header home={false} />
+                <Form />
+                <Footer />
+              </>
+            )}
+            exact
+          />
+          <Route
+            path="/advice/:user"
+            render={() => (
+              <>
+                <Header home={false} />
+                <Prescription />
+                <Footer />
+              </>
+            )}
+            exact
+          />
+          <Route
+            path="/about"
+            render={() => (
+              <>
+                <Header home={false} />
+                <About />
+                <Footer />
+              </>
+            )}
+            exact
+          />
+          <Route
+            path="/solution-index"
+            render={() => (
+              <>
+                <Header home={false} />
+                <SolutionIndex />
+                <Footer />
+              </>
+            )}
+            exact
+          />
+          <Route render={() => (
+            <>
+              <Header home={false} />
+              <div className="error-wrapper">
+                <ErrorPage message={"We don't have a page here."}/>
+              </div>
+            </>
+          )}
+          />
+          </Switch>
+        </>
+  )
+}
+export default App
