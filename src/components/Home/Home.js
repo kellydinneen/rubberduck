d980b645446459dce577635bcf552a8865019b80
kellydinneen <kgdinneen@gmail.com>
2021-03-06 21:15:51 -0700

    Add duck to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import Duck from '../Duck/Duck';
 import './Home.css';
 
 const Home = (props) => {
 
   return (
     <main>
+      <Duck />
     </main>
   )
 }
