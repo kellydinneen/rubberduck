82df0bbb60b0d1d40a7077e68eeb6f65c8fbb6ba
kellydinneen <kgdinneen@gmail.com>
2021-03-06 21:12:12 -0700

    Outline home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import './Home.css';
+
+const Home = (props) => {
+
+  return (
+    <main>
+    </main>
+  )
+}
+
+export default Home;
